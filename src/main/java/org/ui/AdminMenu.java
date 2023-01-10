46ab03b24a6c30444674bcf501668c2d176e7069
royjonessantoso1703 <royjosan17@gmail.com>
2023-01-10 15:06:37 +0700

    admin

--- src/main/java/org/ui/AdminMenu.java
+++ src/main/java/org/ui/AdminMenu.java
@@ -1,8 +1,27 @@
 package org.ui;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 
 public class AdminMenu {
-    private JButton button1;
+    private JButton ADDDATAButton;
     private JPanel panel1;
+    private JButton DELETEButton;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTable table1;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTable tableUpdate;
+
+    private void jTableMouseClicked(java.awt.event.MouseEvent evt) {
+        //to do add your handling code here:
+        //set data to their textfield
+
+        DefaultTableModel tblModel = (DefaultTableModel)table1.getModel();
+
+        //set data to text field when raw is selected
+
+        String tblKodeProduk = tblModel.getValueAt(table1.getSelectedRow(), 0) .toString();
+    }
 }
