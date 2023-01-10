a5be06c77120ce9616aeae687492581f5b2cb8fa
RaffiN2002 <naufalr2020@gmail.com>
2023-01-10 15:44:47 +0700

    Form CheckoutKonfirmasi

--- src/main/java/org/ui/CheckoutKonfirmasi.java
+++ src/main/java/org/ui/CheckoutKonfirmasi.java
@@ -4,4 +4,10 @@ import javax.swing.*;
 
 public class CheckoutKonfirmasi {
     private JPanel panel1;
+    private JTextPane textPane1;
+    private JTextPane textPane2;
+    private JComboBox comboBox1;
+    private JButton pesanButton;
+    private JTextPane textPane3;
+    private JTextField pesananTextField;
 }
